7bbaaed36d0e5f38a1e9856173a811aa109f8923
Robert <bobsaidhi01@gmail.com>
2024-09-19 18:09:09 -0400

    Fixed Init

--- .venv/Checkings_Account.py
+++ .venv/Checkings_Account.py
@@ -1,7 +1,7 @@
 import Bank
 class Checkings_Account(Bank.Bank):
     def __init__(self,name, bal, minBal,acc_num,route_num,trans_limit):
-        super.__init__(name, bal, minBal,acc_num,route_num)
+        super().__init__(name, bal, minBal,acc_num,route_num)
         self.transfer_limit = trans_limit
 
     def withdraw(self, amount):
--- .venv/Savings_Account.py
+++ .venv/Savings_Account.py
@@ -1,5 +1,5 @@
 import Bank
 class Savings_Account(Bank.Bank):
     def __init__(self,name, bal, minBal,acc_num,route_num, interest):
-        super.__init__(name, bal, minBal,acc_num,route_num)
+        super().__init__(name, bal, minBal,acc_num,route_num)
         self.interest_rate = interest
