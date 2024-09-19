760508eeb3f0551ff3cec7c1d3f8625920b2b416
Robert <bobsaidhi01@gmail.com>
2024-09-19 18:10:09 -0400

    Fixed Main Savings Declaration

--- .venv/Main.py
+++ .venv/Main.py
@@ -2,8 +2,8 @@ import Checkings_Account
 import Savings_Account
 Checking_1 = Checkings_Account.Checkings_Account("Steve",1000,500, 1234, 5678,750)
 Checking_2 = Checkings_Account.Checkings_Account("Kevin",1500,250, 2345, 6789,500)
-Saving_1 = Savings_Account.Saving_Account("Sarah",1250,750, 3456, 7890,1.01)
-Saving_2 = Savings_Account.Saving_Account("Betsy",2500,500, 4567, 8901,1.05)
+Saving_1 = Savings_Account.Savings_Account("Sarah",1250,750, 3456, 7890,1.01)
+Saving_2 = Savings_Account.Savings_Account("Betsy",2500,500, 4567, 8901,1.05)
 Checking_1.deposit_check(1000)
 Checking_1.withdraw(750)
 Checking_1.print_customer_information()
