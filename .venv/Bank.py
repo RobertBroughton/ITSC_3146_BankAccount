398082fbac2d52d846f260192a34f31d3c968105
Robert <bobsaidhi01@gmail.com>
2024-09-19 17:12:26 -0400

    Changed to Modules with Checkings_Account.py & Savings_Account.py

--- .venv/Bank.py
+++ .venv/Bank.py
@@ -13,15 +13,3 @@ class Bank:
             self.current_balance -= amount
     def print_customer_information(self):
         print("Bank Title: ",self.title_of_bank,"\nCustomer Name: ",self.customer_name,"\nCurrent Balance: ",self.current_balance, "\nMinimum Balance: ",self.minimum_balance, "\n")
-
-class Savings_Account(Bank):
-    def __init__(self,name, bal, minBal,Acc_num,route_num):
-        super.__init__(name, bal, minBal)
-        self._account_number = Acc_num
-        self.__routing_number = route_num
-
-class Checkings_Account(Bank):
-    def __init__(self,name, bal, minBal,Acc_num,route_num):
-        super.__init__(name, bal, minBal)
-        self._account_number = Acc_num
-        self.__routing_number = route_num
---
+++ .venv/Checkings_Account.py
@@ -0,0 +1,6 @@
+import Bank
+class Checkings_Account(Bank.Bank):
+    def __init__(self,name, bal, minBal,Acc_num,route_num):
+        super.__init__(name, bal, minBal)
+        self._account_number = Acc_num
+        self.__routing_number = route_num
