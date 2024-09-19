3e6c4b62560b398f7203057226bccb9b9afcea45
Robert <bobsaidhi01@gmail.com>
2024-09-19 18:21:20 -0400

    Fixed Main & updated Checkings_Account.py

--- .venv/Checkings_Account.py
+++ .venv/Checkings_Account.py
@@ -3,9 +3,9 @@ class Checkings_Account(Bank.Bank):
     def __init__(self,name, bal, minBal,acc_num,route_num,trans_limit):
         super().__init__(name, bal, minBal,acc_num,route_num)
         self.transfer_limit = trans_limit
-
+    #overloaded Method
     def withdraw(self, amount):
-        if self.current_balance - amount < self.minimum_balance & amount <= self.transfer_limit:
+        if self.current_balance - amount < self.minimum_balance & amount > self.transfer_limit:
             print("Error: Insufficient Funds Available")
         else:
             self.current_balance -= amount
