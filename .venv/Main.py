9642504ab21ca51ee7f532fb3a26394c5f5bafe4
Robert <bobsaidhi01@gmail.com>
2024-09-19 17:58:48 -0400

    Added Main & Fixed Withdraw Method

---
+++ .venv/Main.py
@@ -0,0 +1,6 @@
+import Checkings_Account
+import Savings_Account
+Checking_1 = Checkings_Account.Checkings_Account("Steve",1000,500, 1234, 5678,"")
+Checking_2 = Checkings_Account.Checkings_Account()
+Saving_1 = Savings_Account.Saving_Account()
+Saving_2 = Savings_Account.Saving_Account()
