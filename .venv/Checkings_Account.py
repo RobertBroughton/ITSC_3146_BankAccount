77a5af26d9ee91e5a6cbf501173a57e885c5b599
Robert <bobsaidhi01@gmail.com>
2024-09-19 17:40:40 -0400

    Added Overloaded Withdraw Method

--- .venv/Checkings_Account.py
+++ .venv/Checkings_Account.py
@@ -3,3 +3,9 @@ class Checkings_Account(Bank.Bank):
     def __init__(self,name, bal, minBal,acc_num,route_num,trans_limit):
         super.__init__(name, bal, minBal,acc_num,route_num)
         self.transfer_limit = trans_limit
+
+    def withdraw(self, amount):
+        if self.current_balance - amount < self.transfer_limit:
+            print("Error: Transfer Limit exceeded")
+        else:
+            self.current_balance -= amount
