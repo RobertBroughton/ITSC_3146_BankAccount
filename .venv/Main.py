43ced4092d09ccc0a17113abd30c661cb83ca6f4
Robert <bobsaidhi01@gmail.com>
2024-09-19 18:11:03 -0400

    Fixed Main deposit function

--- .venv/Main.py
+++ .venv/Main.py
@@ -4,7 +4,7 @@ Checking_1 = Checkings_Account.Checkings_Account("Steve",1000,500, 1234, 5678,75
 Checking_2 = Checkings_Account.Checkings_Account("Kevin",1500,250, 2345, 6789,500)
 Saving_1 = Savings_Account.Savings_Account("Sarah",1250,750, 3456, 7890,1.01)
 Saving_2 = Savings_Account.Savings_Account("Betsy",2500,500, 4567, 8901,1.05)
-Checking_1.deposit_check(1000)
+Checking_1.deposit(1000)
 Checking_1.withdraw(750)
 Checking_1.print_customer_information()
 checking_2.deposit(100)
